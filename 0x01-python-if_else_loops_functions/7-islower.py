2390bb0600dc7ac6b501cb1bc95e36d16266a64a
Yvan-David <irankundayvan2020@gmail.com>
2022-09-21 16:35:40 +0200

    python_loop

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from turtle import up
-
-
 def islower(c):
-    n = ord(c)
-    if n in range(97, 123):
-        return True
+    if c + 'a' is True:
+        n = ord(c)
+        if n in range(97, 123):
+            return True
+        else:
+            return False
     else:
         return False
