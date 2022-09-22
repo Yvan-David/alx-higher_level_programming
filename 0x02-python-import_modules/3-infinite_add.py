1ca9cd7983a7490f77c651964ffea173b3af9834
Yvan-David <irankundayvan2020@gmail.com>
2022-09-22 12:50:46 +0200

    module

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print("0")
+elif len(argv) == 2:
+    print("{}".format(argv[1]))
+else:
+    s = 0
+    for i in range(1, (len(argv))):
+        s = s + int(argv[i])
+    print("{}".format(s))
