0ddaf86044d80851bfd09b40e99efbe558d88cb5
Yvan-David <irankundayvan2020@gmail.com>
2022-09-23 10:09:06 +0200

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     b = 2
     #print(c(a,b))
     if  (type(a)) == int and (type(b) == int): 
-        print("{} + {} = {}".format(a, b, c(a, b)))
+        print(f'{a} + {b} = {c.add(a,b)}')
