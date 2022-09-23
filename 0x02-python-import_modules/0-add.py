4d6a8ee9ca903bdd82631449518c6fb059d331d3
Yvan-David <irankundayvan2020@gmail.com>
2022-09-23 09:01:59 +0200

    readme

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add
-a = 1
-b = 2
-c = add(a, b)
-print("{} + {} = {}".format(a, b, c))
+if __name__ == '__main__':
+    from calculator_1 import add
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
