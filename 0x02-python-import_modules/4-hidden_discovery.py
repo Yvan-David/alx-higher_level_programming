8ee689a116d119eedd2b9ce025cac05a84e09398
Yvan-David <irankundayvan2020@gmail.com>
2022-09-23 12:40:59 +0200

    module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+    import sys
     import hidden_4
+if __name__ == '__main__':
     for name in dir(hidden_4):
         if name[:2] != '_':
             print(name)
