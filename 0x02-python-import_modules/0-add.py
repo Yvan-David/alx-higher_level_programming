6282877248ef15ca932b50fc9739a9a8bba87f3e
Yvan-David <irankundayvan2020@gmail.com>
2022-09-23 09:55:11 +0200

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from add_0 import add as c
+    import add_0 as c
     a = 1
     b = 2
     #print(c(a,b))
     if  (type(a)) == int and (type(b) == int): 
-        print("{} + {} = {}".format(a, b, c(a, b)))
+        print("{} + {} = {}".format(a, b, c.add(a, b)))
