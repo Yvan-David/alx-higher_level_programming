3bd842930198db480b165d682e36a80f6ba25f2a
Yvan-David <irankundayvan2020@gmail.com>
2022-09-23 10:12:16 +0200

    module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,5 @@ if __name__ == '__main__':
     import add_0 as c
     a = 1
     b = 2
-    #print(c(a,b))
     if  (type(a)) == int and (type(b) == int): 
         print(f'{a} + {b} = {c.add(a,b)}')
