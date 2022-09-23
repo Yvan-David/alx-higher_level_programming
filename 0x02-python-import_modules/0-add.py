6c7b0751e28edfa517741e4245eec5b455b6a25f
Yvan-David <irankundayvan2020@gmail.com>
2022-09-23 09:09:35 +0200

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import calculator_1
+    add = calculator_1.add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
