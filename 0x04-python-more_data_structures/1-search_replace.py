67c6ccfce53cbeddac0fe485c815e3805f25762a
Yvan-David <irankundayvan2020@gmail.com>
2022-09-30 10:44:51 +0200

    more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,8 @@ def search_replace(my_list, search, replace):
     if my_list is None:
         return
     new_list = my_list.copy()
-    if search in new_list:
-        i = new_list.index(search)
-        new_list[i] = replace
+    for n in new_list:
+        if n == search:
+            i = new_list.index(n)
+            new_list[i] = replace
     return (new_list)
