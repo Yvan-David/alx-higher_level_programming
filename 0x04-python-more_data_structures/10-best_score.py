3e6c98650a14b75e4bf913afedaca34a0ebe7e1b
Yvan-David <irankundayvan2020@gmail.com>
2022-10-03 15:00:40 +0200

    more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     n = 0
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return
     for i, v in a_dictionary.items():
         if v > n:
