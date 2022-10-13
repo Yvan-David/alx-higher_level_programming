2b076e192926abd85fc64eca34233f84d487a22b
Yvan-David <irankundayvan2020@gmail.com>
2022-10-13 08:01:49 +0200

    exceptions_and_errors_in_python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    v = []
-    for i in my_list:
-        try:
-            v.append(i)
-            if (my_list.index(i) + 1 == x):
-                raise TypeError
-        except TypeError:
-            break
-    for i in v:
-        print("{}".format(i),end='')
-    print('')
+    try:
+        v = []
+        c = []
+        for i in my_list:
+            try:
+                v.append(i)
+                if (my_list.index(i) + 1 == x):
+                    raise TypeError
+            except TypeError:
+                break
+        for i in v:
+            print("{}".format(i), end='')
+        print('')
+        if ((v == c) or x == 0):
+            raise TypeError
+    except TypeError:
+        return (0)
     return (v.index(i) + 1)
