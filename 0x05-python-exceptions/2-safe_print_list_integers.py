5673ad65bbcee51c98cd4358eb2fc03e2815d00b
Yvan-David <irankundayvan2020@gmail.com>
2022-10-17 21:01:49 +0200

    exceptions_and_errors_in_python

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        v = []
+        c = []
+        n = 0
+        for i in my_list:
+            try:
+                v.append(i)
+                if (my_list.index(i) + 1 == x):
+                    raise TypeError
+            except TypeError:
+                break
+        for i in v:
+            try:
+                i + 1
+                print("{}".format(i), end='')
+                #print('')
+                n = n + 1
+            except (TypeError, ValueError):
+                continue
+        print('')
+        if ((v == c) or x == 0):
+            raise TypeError
+    except TypeError:
+        return (0)
+    return (n)
