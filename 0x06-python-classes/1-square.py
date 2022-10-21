83e9692bc7610d2b1307ffe069b532771c649236
Yvan-David <irankundayvan2020@gmail.com>
2022-10-21 15:29:01 +0200

    class_object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is a module that makes a class."""
+
+
+class Square:
+    """this is a class called square."""
+    def __init__(self, __size):
+        """this is a method called """
+        self.__size = __size
