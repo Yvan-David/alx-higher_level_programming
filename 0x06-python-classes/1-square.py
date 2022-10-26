702dcd5fb1a307f8c79c60a8fdb9ea06dadaf148
Yvan-David <irankundayvan2020@gmail.com>
2022-10-26 18:42:28 +0200

    class_object

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,6 @@
 class Square:
     """this is a class called square."""
     def __init__(self, __size):
+        
         """this is a method called """
         self.__size = __size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""this is a module that makes a class."""
+
+
+class Square:
+    """this is a class called square."""
+    def __init__(self, size=0):
+            if (type(size) != int):
+                raise TypeError("size must be an integer")
+            elif (size < 0):
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+                self.size = size
+    # def size(self):
+    #     return (self.size)
+    # def ssize(self, value):
+    #     self.__size = value
+    def area(self):
+        if (type(self.size) != int):
+            raise TypeError("size must be an integer")
+        elif (self.size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            return (self.size ** 2)
+    #self.size = self,__size
