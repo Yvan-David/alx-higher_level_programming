3fdde463146d0e7fe6374b77c589b3fbcca2c8d7
Yvan-David <irankundayvan2020@gmail.com>
2022-11-03 18:07:25 +0200

    test_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+"""
+this is a module that has a function adding int
+"""
+
+
 def add_integer(a, b=98):
-    if (type(a) == int or type(a) == float):
-        if (type(b) == int or type(b) == float):
-            if (type(a) == float):
-                a = int(a)
-            if (type(b) == float):
-                b = int(b)
-            return (a + b)
+    """
+    this is a function that adds int
+    """
+    if (type(a) == float):
+        a = int(a)
+    if (type(b) == float):
+        b = int(b)
+    if ((type(a) == (int or float)) and (type(b) == (int or float))):
+        return (a + b)
     else:
         if (type(a) != int and type(a) != float):
             raise TypeError('a must be an integer')
-        elif (type(b) != int and type(b) != float):
+        if (type(b) != int and type(b) != float):
             raise TypeError('b must be an integer')
