22da94b1d1f2c0cf0b963817c66e74bdc437f30a
Yvan-David <irankundayvan2020@gmail.com>
2022-11-04 10:02:10 +0200

    more_classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+class called rectangle
+"""
+
+
+class Rectangle():
+    """
+    class called rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        class called rectangle
+        """
+
+        if (type(width) == int) and (width >= 0):
+            self.__width = width
+        if (type(height) == int) and (height >= 0):
+            self.__height = height
+        if (type(width) != int):
+            raise TypeError('width must be an integer')
+        if (width < 0):
+            raise ValueError('width must be >= 0')
+        if (type(height) != int):
+            raise TypeError('height must be an integer')
+        if (height < 0):
+            raise ValueError('height must be >= 0')
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
