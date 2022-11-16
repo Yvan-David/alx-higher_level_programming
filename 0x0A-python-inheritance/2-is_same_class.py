19a49b7c41081945fa935b6fbcd9a17ac5a213ea
Yvan-David <irankundayvan2020@gmail.com>
2022-11-16 22:19:00 +0200

    inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a module for isinstance
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    a function for isinstance
+    """
+
+    if (isinstance(obj, a_class)):
+        return (True)
+    else:
+        return (False)
