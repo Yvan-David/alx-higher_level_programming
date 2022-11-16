7fb6c801a380a0075aecfa5ea7a066a065a4a4d7
Yvan-David <irankundayvan2020@gmail.com>
2022-11-16 20:01:17 +0200

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a module for MYlist
+"""
+
+
+class MyList(list):
+    """
+    a class for mylist
+    """
+    def print_sorted(self):
+        """
+    a function for printing sorted mylist
+    """
+        n = self.copy()
+        n.sort()
+        print(n)
