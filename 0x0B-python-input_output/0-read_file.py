4a6c6158b549c7501c871eb746e64d181755c22f
Yvan-David <irankundayvan2020@gmail.com>
2022-11-17 20:25:25 +0200

    file_handling

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module for file reading
+"""
+
+
+def read_file(filename=""):
+    """
+    method for file reading
+    """
+    with open(filename, 'r') as file:
+        print(file.read())
