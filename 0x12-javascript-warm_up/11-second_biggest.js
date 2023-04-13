42aed4997daf03f8af8aca5cfa8390dff2a91be3
Yvan-David <irankundayvan2020@gmail.com>
2023-04-13 11:57:30 +0200

    correction of the codes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
 function search () {
-  let i;
-  let v = 0;
-  const arr = [];
-  for (i = 2; i <= process.argv.length; i++) {
-    if (parseInt(process.argv[i]) < 0) {
-      v = parseInt(process.argv[i]);
+  let i, temp;
+  for (i = 0; i < process.argv.length - 1; i++) {
+    for (let j = 0; j < process.argv.length - 1; j++) {
+      if (process.argv[j] > process.argv[j + 1]) {
+        temp = process.argv[j];
+        process.argv[j] = process.argv[j + 1];
+        process.argv[j + 1] = temp;
+      }
     }
   }
-  for (i = 2; i <= process.argv.length; i++) {
-    if (parseInt(process.argv[i]) > v) {
-      v = parseInt(process.argv[i]);
-      arr.push(v);
-    }
-  }
-  console.log(arr[arr.length - 2]);
+  console.log(process.argv[process.argv.length - 2]);
 }
 if (process.argv.length <= 3) {
   console.log('0');
