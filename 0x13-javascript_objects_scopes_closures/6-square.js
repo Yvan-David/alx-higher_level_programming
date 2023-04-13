3c0c542caf7b7f87c85264d83eb8dc50cf71f80b
Yvan-David <irankundayvan2020@gmail.com>
2023-04-13 19:55:04 +0200

    correct codes

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return;
-    }
-    this.width = w;
-    this.height = h;
-  }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
-      }
-      console.log('');
-    }
-  }
-
-  rotate () {
-    const value = this.width;
-    this.width = this.height;
-    this.height = value;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends require('./5-square') {
   constructor (size) {
     super(size, size);
   }
