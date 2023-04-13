45740efeab85320b489cec724ee13dab3694fc80
Yvan-David <irankundayvan2020@gmail.com>
2023-04-13 18:49:33 +0200

    correct codes

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-class Rectangle
-{
-    constructor(w, h)
-    {
-        if (w <= 0 || h <= 0 || w === undefined || h === undefined)
-        {
-            return;
-        }
-        this.width = w;
-        this.height = h;
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return;
     }
+    this.width = w;
+    this.height = h;
+  }
 }
