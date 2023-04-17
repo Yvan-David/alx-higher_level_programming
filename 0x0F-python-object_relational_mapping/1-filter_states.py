c2d9922ec0de3e7fcb9f31bef961c96085fb1676
Yvan-David <irankundayvan2020@gmail.com>
2023-04-17 15:44:03 +0200

    mysqld trials

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+
+"""
+import MySQLdb
+import sys
+try:
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
+                         database=sys.argv[3])
+    c = db.cursor()
+    c.execute(""" SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id """)
+    for i in c.fetchall():
+        print(i)
+except IndexError:
+    print("no arguments given")
