35159bd981aae13b59628af414535f6698b875f2
Yvan-David <irankundayvan2020@gmail.com>
2023-04-17 11:48:46 +0200

    mysldb print table list

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,11 @@ a script that lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
-
-db = MySQLdb.connect(user=sys.argv[1], database=sys.argv[2])
-c = db.cursor()
-c.execute(""" SELECT id, name FROM states """)
-for i in c.fetchall():
-    print(i)
+try:
+    db = MySQLdb.connect(user=sys.argv[1], database=sys.argv[2])
+    c = db.cursor()
+    c.execute(""" SELECT id, name FROM states """)
+    for i in c.fetchall():
+       print(i)
+except:
+    pass
