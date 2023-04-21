9f8b474e1a2d16f6c1629b8535dc53210e92d900
Yvan-David <irankundayvan2020@gmail.com>
2023-04-21 22:14:59 +0200

    sqlalchemy create table

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module that creates table using sqlalchemy  """
+import sys
+from sqlalchemy import Column, String, INTEGER, UniqueConstraint
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """ State class that creates a states table """
+
+    __tablename__ = "states"
+
+    id = Column(INTEGER, primary_key=True, nullable=False)
+    UniqueConstraint(id)
+    name = Column(String(128), nullable=False)
