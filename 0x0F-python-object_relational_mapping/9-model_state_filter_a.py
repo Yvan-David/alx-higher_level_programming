597cc04c1f3379613c42cf983da4c14ef333c796
Yvan-David <irankundayvan2020@gmail.com>
2023-04-21 22:42:35 +0200

    using session and filter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module that lists state name in table states """
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                       sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+stats = session.query(State).filter(State.name.like('%a%'))
+for stat in stats:
+    print(f'{stat.id}:  {stat.name}')
