e78f11f35511676507d4ff6c78ff1f4abc32edec
Yvan-David <irankundayvan2020@gmail.com>
2023-04-21 23:25:09 +0200

    using session and filter

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module that lists state name in table states """
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                       sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+stats = session.query(State).filter(State.id == 2)
+for stat in stats:
+    stat.name = 'New Mexico'
+session.commit()
