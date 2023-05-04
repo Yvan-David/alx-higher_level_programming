12cd1cadb94a34e63a1ad33415ea86c3adc5e132
Yvan-David <irankundayvan2020@gmail.com>
2023-05-04 16:01:52 +0200

    network basics

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # a script that takes the url and display some of its messages
-curl -is $1 -o save
-cat save | wc -c
+
+curl -is $1 -o saved
+cat saved | grep Content-Length | cut -d ' ' -f 2
