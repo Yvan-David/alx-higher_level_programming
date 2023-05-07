220b713dfa3db36b2a257dcf87d351410ad0bee6
Yvan-David <irankundayvan2020@gmail.com>
2023-05-07 21:00:37 +0200

    urllib.request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module that request url and reads content in bytes and html """
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")\
+         as response:
+        html = response.read()
+    print(f"Body response:\n\
+       - type: {type(html)}\n\
+       - content: {html}\n\
+       - utf8 content: {html.decode('UTF-8')}")
