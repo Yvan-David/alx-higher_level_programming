48f374c66ebdc465a6dc1800fb778069c6be78e9
Yvan-David <irankundayvan2020@gmail.com>
2023-05-08 20:05:18 +0200

    get header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,4 +7,4 @@ import requests
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     if r.headers['X-Request-Id']:
-        print((r.headers['X-Request-Id']))
+        print((r.headers.get('X-Request-Id')))
