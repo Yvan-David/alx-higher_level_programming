83d5fce5d9e76316169a485c690d1e4b536272ac
Yvan-David <irankundayvan2020@gmail.com>
2023-05-08 18:27:27 +0200

    Post request

---
+++ 0x11-python-network_1/2-post_email2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module that POST an email and display response body
+    and prints the body of the response in html
+"""
+
+from sys import argv
+import urllib.request
+
+if __name__ == '__main__':
+    dat = {"email": argv[2]}
+    data = urllib.parse.urlencode(dat)
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
