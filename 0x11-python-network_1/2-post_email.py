c3064643d01c5c74fdb37c5dbf492ad0b02d5326
Yvan-David <irankundayvan2020@gmail.com>
2023-05-08 17:50:42 +0200

    Post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,12 +2,17 @@
 """ module that POST an email and display response body """
 
 if __name__ == '__main__':
-    import sys
-    import urllib.request
-
-    data = f'email={sys.argv[2]}'
-    edata = data.encode('ascii')
-    req = urllib.request.Request(sys.argv[1], edata)
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-    print(html.decode('utf8'))
+    try:
+        import sys
+        import urllib.request
+        if sys.argv[2].find('=') != -1:
+            data = sys.argv[2]
+        else:
+            data = f'email={sys.argv[2]}'
+        edata = data.encode('ascii')
+        req = urllib.request.Request(sys.argv[1], edata)
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+        print(html.decode('utf8'))
+    except IndexError:
+        print('please check arguments parsed')
---
+++ 0x11-python-network_1/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+if sys.argv[1].find('=') != -1:
+    print('done')
+else:
+    print(None)
