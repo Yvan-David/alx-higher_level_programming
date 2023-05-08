580b02f9d2c99346f91309efb0fe62cf36cc2e3b
Yvan-David <irankundayvan2020@gmail.com>
2023-05-08 17:59:16 +0200

    Post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-""" module that POST an email and display response body """
+""" takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays
+the body of the response (decoded in utf-8) """
 
-if __name__ == '__main__':
-    try:
-        import sys
-        import urllib.request
-        if sys.argv[2].find('=') != -1:
-            data = sys.argv[2]
-        else:
-            data = f'email={sys.argv[2]}'
-        edata = data.encode('ascii')
-        req = urllib.request.Request(sys.argv[1], edata)
-        with urllib.request.urlopen(req) as response:
-            html = response.read()
-        print(html.decode('utf8'))
-    except IndexError:
-        print('please check arguments parsed')
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    email_dict = {"email": email}
+    email_encode = urllib.parse.urlencode(email_dict)
+    email_encode = email_encode.encode('ascii')
+    request = urllib.request.Request(url, email_encode)
+
+    with urllib.request.urlopen(request) as response:
+        page = response.read()
+        page_decode = page.decode('utf-8')
+        print(page_decode)
--- 0x11-python-network_1/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import sys
-if sys.argv[1].find('=') != -1:
-    print('done')
-else:
-    print(None)
