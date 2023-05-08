255eae5854ee4f7ea1bdc8b7eff4c76e1261d525
Yvan-David <irankundayvan2020@gmail.com>
2023-05-08 20:42:14 +0200

    post and display

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" post url and email address """
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    load = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], load)
+    print(r.text)
