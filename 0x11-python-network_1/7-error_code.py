9756ab8298032e3a4bd3fccceade8542ca9424e8
Yvan-David <irankundayvan2020@gmail.com>
2023-05-08 21:04:40 +0200

    error handling

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" module that handles http error codes """
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
+    code = r.status_code
+    if code >= 400:
+        print(f"Error code : {code}")
+    else:
+        print(r.text)
