fdaafa6c7c64c07c8a2702530f98b94d574f38b4
Yvan-David <irankundayvan2020@gmail.com>
2023-05-09 21:16:51 +0200

    more querries

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- SCRIPT that creates a database and a table on that DB
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
