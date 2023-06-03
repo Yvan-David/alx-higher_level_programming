3d45b1da1fdd7e2368b6f8680c088f226451c067
Yvan-David <irankundayvan2020@gmail.com>
2023-06-03 23:22:51 +0200

    exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -15,6 +15,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except:
+    except TypeError:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
