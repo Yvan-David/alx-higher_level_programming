b8435e11bdbfefe549581d7e1cb977c4c8d5aed1
Yvan-David <irankundayvan2020@gmail.com>
2023-06-04 17:32:22 +0200

    more querries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,9 @@
--- script that uses subquery to of two rlated tables
-SELECT cities.name
-FROM cities
-WHERE cities.state_id=(SELECT state.id FROM states WHERE name=California);
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
